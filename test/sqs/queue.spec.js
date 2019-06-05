240c60898c750527852c1e5c94d591f4f99e72da
Evan Sosenko <evan.sosenko@meltwater.com>
2019-06-05 13:56:40 -0700

    Add test for batchSize

--- test/sqs/queue.spec.js
+++ test/sqs/queue.spec.js
@@ -9,8 +9,9 @@ import { SqsQueue } from '../../lib'
 test.beforeEach(async t => {
   await setupContext(t)
 
-  t.context.queue = (t, handler = x => x) => new SqsQueue({
+  t.context.queue = (t, handler = x => x, opts = {}) => new SqsQueue({
     ...t.context.queueConfig,
+    ...opts,
     handler,
     log: createLogger({ t })
   })
@@ -42,6 +43,26 @@ test('process', async t => {
 
   await queue.start()
   const { Body } = await new Promise(resolve => { event.on('data', resolve) })
+  t.is(Body, msg)
+  await queue.stop()
+})
+
+test('process batch', async t => {
+  t.timeout(3000)
+  const msg = JSON.stringify({ a: 2 })
+  const event = new EventEmitter()
+  const handler = m => { event.emit('data', m) }
+  const queue = t.context.queue(t, handler, { batchSize: 5 })
+  await Promise.all([1, 2, 3, 4, 5].map(() => queue.publish(msg, { json: false })))
+
+  await queue.start()
+  const { Body } = await new Promise(resolve => {
+    let n = 0
+    event.on('data', data => {
+      n++
+      if (n === 5) resolve(data)
+    })
+  })
   t.is(Body, msg)
   await queue.stop()
 })
